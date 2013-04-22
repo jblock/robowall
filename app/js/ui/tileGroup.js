96ca5b2385541d1e19756d5beb95e111fa0b860e
Kevin Schaefer <kevinjschaefer@gmail.com>
2013-04-22 02:20:34 -0400

    Added basic layout generation. CSS changes.

--- app/js/ui/tileGroup.js
+++ app/js/ui/tileGroup.js
@@ -19,17 +19,42 @@ define(
 			});
 
 			this.renderAll = function(e, data) {
-				console.log(data);
+				// Create a default layout recipe
+				var layouts = [{ '1x1': 4, '2x1': 1, '2x2': 1, '3x2': 1 },
+							   { '1x1': 6, '2x1': 3, '2x2': 1, '3x2': 0 },
+							   { '1x1': 4, '2x1': 2, '2x2': 2, '3x2': 0 }];
+				var rand = Math.floor(Math.random()*3);
+
 				this.$node.html('');
+				// Todo: sort the articles by routine & popularity
 				data.articles.forEach(function(article) {
-					this.render(e, {tile: article});
+					if (layouts[rand]['3x2'] > 0) {
+						article.size = '3x2';
+						layouts[rand]['3x2'] --;
+						this.render(e, {tile: article});
+					} else if (layouts[rand]['2x2'] > 0) {
+						article.size = '2x2';
+						layouts[rand]['2x2'] --;
+						this.render(e, {tile: article});
+					} else if (layouts[rand]['2x1'] > 0) {
+						article.size = '2x1';
+						layouts[rand]['2x1'] --;
+						this.render(e, {tile: article});
+					} else if (layouts[rand]['1x1'] > 0) {
+						article.size = '1x1';
+						layouts[rand]['1x1'] --;
+						this.render(e, {tile: article});
+					}
 				}, this);
+
+				// Compute the optimal arrangement
 				var container = document.querySelector('#tileContainer');
 				var pckry = new Packery( container, {
 				  // options
 				  itemSelector: '.tile',
-				  gutter: 10,
-				  columnWidth: 245
+				  gutter: 20,
+				  columnWidth: 245,
+				  containerStyle: null
 				});
 			}
 
