c84ce9bf949f893869da30aa5890e76285738bce
jblock <jasontheblock@gmail.com>
2013-05-24 00:03:09 -0400

    prototyping state

--- app/js/app.js
+++ app/js/app.js
@@ -13,13 +13,13 @@ define(
 		var initialize = function() {
 			Controller.attachTo(document);
 			TwitterBlurb.attachTo(document);
-			Visualizer.attachTo('#screen1 .threeContainer');
-			FeaturedTile.attachTo('#screen1 .featuredTileContainer');
-			TileGroup.attachTo('#screen1 .tileContainer');
+			Visualizer.attachTo('#screen2 .threeContainer');
+			// FeaturedTile.attachTo('#screen1 .featuredTileContainer');
+			// TileGroup.attachTo('#screen1 .tileContainer');
 
 			// Visualizer.attachTo('#screen2 .threeContainer');
-			// FeaturedTile.attachTo('#screen2 .featuredTileContainer');
-			// TileGroup.attachTo('#screen2 .tileContainer');
+			FeaturedTile.attachTo('#screen2 .featuredTileContainer');
+			TileGroup.attachTo('#screen2 .tileContainer');
 
 			// FeaturedTile.attachTo('#screen3 .featuredTileContainer');
 			// TileGroup.attachTo('#screen3 .tileContainer');
